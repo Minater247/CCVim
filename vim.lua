f47af0528fe1065c453e6f9576aaae224ca81081
Minater247 <nreed132@gmail.com>
2022-10-01 20:21:51 -0700

    Fix nil error when drawing unsyntaxed filetypes

--- vim.lua
+++ vim.lua
@@ -11,11 +11,15 @@ local function newBuffer(path)
     local buf = {}
     buf.lines = {text = fil.toArray(path)}
     buf.filetype = str.getFileExtension(path)
-    buf.highlighter = require("/vim/syntax/" .. buf.filetype)
+    if fs.exists("/vim/syntax/" .. buf.filetype..".lua") then
+        buf.highlighter = require("/vim/syntax/" .. buf.filetype)
+    end
 
     if vars.syntax and buf.highlighter then
         buf.lines.syntax = buf.highlighter.parseSyntax(buf.lines.text)
     end
+
+    buf.cursorX, buf.cursorY = 1, 1
 
     return buf
 end
@@ -28,7 +32,7 @@ end
 
 
 
-local buf = newBuffer("/vim/vim.lua")
+local buf = newBuffer("/out.test")
 local xpos = 1
 term.clear()
 
@@ -38,19 +42,26 @@ local wid, hig = term.getSize()
 local ff = fs.open("/out.test", "w")
 ff.write(textutils.serialise(buf.lines.syntax))
 ff.close()
-for i=1, #buf.lines.syntax do
-    local locali = i
-    if i > hig then
-        term.scroll(1)
-        locali = hig
-    end
+if buf.lines.syntax then
+    for i=1, #buf.lines.syntax do
+        local locali = i
+        if i > hig then
+            term.scroll(1)
+            locali = hig
+        end
 
-    local xpos = 1
-    for j=1, #buf.lines.syntax[i] do
-        term.setCursorPos(xpos, locali)
-        term.setTextColor(buf.lines.syntax[i][j].color)
-        term.write(buf.lines.syntax[i][j].string)
-        xpos = xpos + #buf.lines.syntax[i][j].string
+        local xpos = 1
+        for j=1, #buf.lines.syntax[i] do
+            term.setCursorPos(xpos, locali)
+            term.setTextColor(buf.lines.syntax[i][j].color)
+            term.write(buf.lines.syntax[i][j].string)
+            xpos = xpos + #buf.lines.syntax[i][j].string
+        end
+    end
+else
+    for i=1, #buf.lines.text do
+        term.setCursorPos(1, i)
+        term.write(buf.lines.text[i])
     end
 end
 term.setCursorPos(1, hig)
